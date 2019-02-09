8f0669fb5240a01bd0329add60bacebecb58418e
Aaron Paterson <juniha.aaron@gmail.com>
2019-02-09 14:33:46 -0500

    updating repo with new code

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// This is the intro to javascript. 
+// To create go to 'View' > 'Javascript'  and save the file to the folder.
+
+
+// create a variable with a string call it a 'name.'
+// Below is a example of Concatination
+//var firstName = "Aaron";
+// alert("Hello" + firstName);
+
+// Modern Js
+// alert(`Hello ${firstName}`);
+
+//console.log("Hello " + firstName);
+
+console.log("javascript linked up");
